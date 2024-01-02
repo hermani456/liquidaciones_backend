c1717415feeca10dfb420af5ed27219cbfa73a9a
hermani456 <hermani457@gmail.com>
2024-01-02 00:49:54 -0300

    update port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const {
   getTrabajador,
   setTrabajador,
